3457c6645ddaf9a82822961b81cb5cbcf2bd2f84
fredrik62 <hogstrom10@gmail.com>
2018-07-02 19:28:17 +0300

    todo redux-angular-done

--- src/app/todo-overview/todo-overview.component.ts
+++ src/app/todo-overview/todo-overview.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgRedux, select} from '@angular-redux/store';
+import { IAppState } from '../store';
+import { REMOVE_ALL_TODOS } from '../actions';
 
 @Component({
   selector: 'app-todo-overview',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-overview.component.css']
 })
 export class TodoOverviewComponent implements OnInit {
-
-  constructor() { }
+@select() todos;
+@select() lastUpdate;
+  constructor(private ngRedux: NgRedux<IAppState>) { }
 
   ngOnInit() {
   }
 
+  clearTodos(){
+    this.ngRedux.dispatch({type: REMOVE_ALL_TODOS})
+  }
 }
